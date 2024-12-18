88d44edeb81fbd1eccc2841f405d927cda27cfa9
YachavarapuKarthik <karthikyacharapu@gmail.com>
2024-12-18 13:56:32 +0530

    Done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Loader from "./components/Loader.tsx"
-import Header from './components/header/Header.tsx';
-import Footer from './components/footer/Footer.tsx';
 import Home from './components/Home.tsx';
 
 const App: React.FC = () => {
