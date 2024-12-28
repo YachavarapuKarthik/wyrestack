32c68734e0d5fc0b6e8276dd166c8b7e95f4c141
YachavarapuKarthik <karthikyacharapu@gmail.com>
2024-12-28 11:31:34 +0530

    Course Page is added

---
+++ client/src/components/courses/CourseHome.tsx
@@ -0,0 +1,11 @@
+function CourseHome(){
+    return (
+        <>
+        <br /> <br /> <br /> <br />
+        <h1 style={{color:'white'}}>Hii</h1>
+        </>
+        
+    );
+}
+
+export default CourseHome;
---
+++ client/src/components/courses/CoursesLanding.tsx
@@ -0,0 +1,14 @@
+import NavBar from "../header/NavBar";
+import CourseHome from "./CourseHome";
+
+function CoursesLanding(){
+    return (
+        <>
+        <NavBar/>
+        <CourseHome/>
+        </>
+        
+    );
+}
+
+export default CoursesLanding;
