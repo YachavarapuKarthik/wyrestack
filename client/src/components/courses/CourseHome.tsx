7e8c0eb1a7cb2495b34aaca6259af5d8bcad362a
chavabhargav <151426466+chavabhargav@users.noreply.github.com>
2024-12-29 11:34:50 +0530

    course1

--- client/src/components/courses/CourseHome.tsx
+++ client/src/components/courses/CourseHome.tsx
@@ -1,8 +1,19 @@
+import "../../css/coursepage-css/coursehome.css";
+
 function CourseHome(){
     return (
         <>
-        <br /> <br /> <br /> <br />
-        <h1 style={{color:'white'}}>Hii</h1>
+        
+        
+        <div className="course-container">
+            <div className="course-info">
+                <div className="course-left">
+                    <h2>Learn software technologies online with us!</h2>
+                    <p>Unlock your potential with Wyrestack! Explore expert-led courses in coding, design, and more. Build skills, gain confidence, and achieve your career goals with our flexible e-learning platform.</p>
+                </div>
+                <div className="course-right"></div>
+            </div>
+        </div>
         </>
         
     );
